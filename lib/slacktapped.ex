6feef55b363b0cc425580f678ff39349de74644e
Jeroen Knoops <jeroen.knoops@philips.com>
2016-10-22 21:42:06 +0200

    add redis port and pwd

--- lib/slacktapped.ex
+++ lib/slacktapped.ex
@@ -12,14 +12,16 @@ defmodule Slacktapped do
   def start(_type, _args) do
     cowboy_port = Application.get_env(:slacktapped, :cowboy_port, 8000)
     redis_host = System.get_env("REDIS_HOST") || "localhost"
-    redis_port = Application.get_env(:slacktapped, :redis_port, 6379)
+    redis_port = System.get_env("REDIS_PORT") || 6379 
+    redis_pwd = System.get_env("REDIS_PWD") || ""
 
     children = [
       Plug.Adapters.Cowboy.child_spec(:http, Slacktapped.Router, [], port: cowboy_port),
       worker(Redix, [
         [
           host: redis_host,
-          port: redis_port
+          port: redis_port,
+          password: redis_pwd
         ],
         [name: :redix]
       ])
